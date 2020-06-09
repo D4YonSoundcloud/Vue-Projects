c8afc93d2b07e07d8d522fbd000471885e671435
d4yonsouncloude <mattd4y@gmail.com>
2020-06-09 17:00:48 -0600

    dynamicCSSwithVue 7:00pm 6-9-20

---
+++ dynamic-styling-css/app.js
@@ -0,0 +1,25 @@
+new Vue({
+  el: "#app",
+  data: {
+    attachRed: false,
+    attachGreen: false,
+    attachBlue: false,
+    color: "green",
+    color2: "grey",
+    width: 50,
+  },
+  computed: {
+    divClasses: function () {
+      return {
+        red: this.attachRed,
+        blue: !this.attachRed,
+      };
+    },
+    myStyle: function () {
+      return {
+        backgroundColor: this.color2,
+        width: this.width + "px",
+      };
+    },
+  },
+});
